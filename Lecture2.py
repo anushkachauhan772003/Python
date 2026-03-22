410729e3cca907227794f8a43bc5f69bb048e433
anushkachauhan772003 <anushkachauhan772003@gmail.com>
2026-03-22 19:56:11 +0530

    Initial commit

---
+++ Lecture2.py
@@ -0,0 +1,33 @@
+# str = "anu",
+# "this is anushka's 2 lecture"
+# str1= "this is anushka.\n And I am studying python"
+# print(str1)
+# str1="anushka"
+# str2="chauhan"
+# print(len(str1+str2))
+# str="apna college"
+# ch=str[0]
+# print(ch) #we cannot change the character of the string, we can access not manipulate
+# print(str[5:])
+# print(str.replace("college", "career"))
+# #lets practice input users first name and write length
+# name=input("enter you name: ")
+# print(len(name))
+# dollar=input("write any sentence containing dollar: ")
+# print(dollar.count("$"))
+#conditional statements
+light="green"
+if(light=="red"):#always check this situation
+    print("strop")#indentation-->proper spacing
+elif(light=="yellow"):#only checked when if statement is wrong
+    print("look")
+else:
+    print("go")
+#write if a no. is odd or even
+num=int(input("enter a value"))
+if(num%2==0):
+    print("even")
+else:
+    print("ODD")
+
+
