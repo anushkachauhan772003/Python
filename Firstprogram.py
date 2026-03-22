b2dadf469314244a06718f4b15e965ac24d1c9c8
anushkachauhan772003 <anushkachauhan772003@gmail.com>
2026-03-22 15:48:55 +0530

    Initial commit

---
+++ Firstprogram.py
@@ -0,0 +1,26 @@
+# Name= "anushka" #String
+# age = 23
+# ctc = None
+# a=2
+# a+=20
+# b=30
+# val1 = True; val2=False
+# # print(type(Name))
+# # print(type(age))
+# # print(type(ctc))
+# # print(a) 
+# print(not(a>b))
+# print(val1 and val2)
+# name = input("enter your name: ")
+# age=int(input("enter your age"))
+# print("welcome ", name)
+# print("your age", age)
+
+#take input and 2 number
+num1=int(input("type num1 "))
+num2=int(input("type num2 "))
+print(num1+num2, type(num2+num1))
+
+#to inout side of a square and print its area
+side1=int(input("enter side of a squre: "))
+print("area= ", side1*side1)
