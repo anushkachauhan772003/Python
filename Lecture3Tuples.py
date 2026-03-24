65e7aea6c95e154d84ffabe42d505f8eb22ec7f9
anushkachauhan772003 <anushkachauhan772003@gmail.com>
2026-03-24 22:43:10 +0530

    Tuples basics

---
+++ Lecture3Tuples.py
@@ -0,0 +1,25 @@
+#Tuples in python is immutable
+# tup=(1,2,3,4)
+# tup1=(1)
+# print(tup[0])
+# print(type(tup1))
+# print(tup[2])
+# print(tup.index())
+#wap to ask the user to enter names
+# movie1=input()
+# movie2=input()
+# movie3=input()
+# Movies=[]
+# Movies.append(movie1)
+# Movies.append(movie2)
+# Movies.append(movie3)
+# print(Movies)
+# list1=[1,2,3]
+# copy_l=list1.copy()
+# copy_l.reverse()
+# if(list1==copy_l):
+#     print("palindrome")
+# else:
+#     print("not palindrome")
+tup=('c','a','b','c','d','e','c')
+print(tup.count('c'))
