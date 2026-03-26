8cc8d3858f2635abf059053f5cb9f0a9cfb3de29
anushkachauhan772003 <anushkachauhan772003@gmail.com>
2026-03-26 22:42:19 +0530

    for loops

---
+++ Lecture5FORloop.py
@@ -0,0 +1,42 @@
+list=[1,2,3,4,5,6]
+veggies=["potatoes", "spinach", "cucumber", "tomato", "coriander"]
+tup=(44,4,34,56,67,78,68)
+for val in tup:
+    print(val)
+#wap to write a squares from 1-10 using for loop
+i=1
+for i in range(10):
+    print ("square of ",i,"=",i*i)
+    i+=1
+# for i in range(2,10):# range(start,stop)
+#     print(i)
+
+#wap to get table
+# n=int(input("enter the no. you want table for"))
+# i=1
+# for i in range(1,11):
+#     print(n*i)
+#     i+=1
+# a=int(input("from where you want sum of 5 natural number: "))
+# counter=1
+# while(counter<5):
+#     a=a+counter
+#     counter+=1
+# print("final sum =", a)
+
+# a=int(input())
+# sum=0
+# for i in range(1,a+1):
+#     sum+=i
+# print(sum)
+
+# Wap to find the factorial of n
+
+b=int(input("write the no. you want factorial: "))
+fact=1
+for i in range (1,b+1):
+    fact=fact*i
+    sum=fact
+
+print(sum)
+
